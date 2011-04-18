7c2965f62079955065aa3dbb52eeb6be390cef8d
Zach Denton <zacharydenton@gmail.com>
2011-04-18 13:47:12 -0400

    make a note of the content source

--- autoblog/sources/rss.py
+++ autoblog/sources/rss.py
@@ -15,12 +15,16 @@ class RssSource(ContentSource):
         for feed_url in self.feed_urls:
             print "processing %s" % feed_url
             doc = feedparser.parse(feed_url)
+            source = doc.feed.link
             for e in doc.entries:
+                url = e.link
                 try:
                     yield Content(
                         title=e.title,
                         content=e.content[0].value,
                         time=datetime.datetime(*e.date_parsed[:6]),
+                        source=source,
+                        url=url,
                     )
                 except Exception as e:
                     pass

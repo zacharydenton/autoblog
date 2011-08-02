7280336981c5b39312e1824c2181f2841d40621c
Zach Denton <zacharydenton@gmail.com>
2011-08-01 20:32:57 -0400

    use octopress

--- autoblog/conf/site_template/js/plugins.js
+++
@@ -1,9 +0,0 @@
-
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  arguments.callee = arguments.callee.caller;  
-  if(this.console) console.log( Array.prototype.slice.call(arguments) );
-};
--- autoblog/conf/site_template/settings.py
+++ autoblog/conf/site_template/settings.py
@@ -6,8 +6,8 @@ import jinja2
 DEBUG = True
 
 feed_urls = [
-    'http://wordpress.org/news/feed/',
-]
+              'http://www.nordiclightpacking.com/feeds/posts/default',
+              'http://www.trailblaze-trekking.com/feed']
 
 SOURCES = [
     autoblog.sources.rss.RssSource(feed_urls),
@@ -15,19 +15,19 @@ SOURCES = [
 
 FILTERS = [
     autoblog.filters.seo.NoFollow(),
-    autoblog.filters.rewrite.GoogleTranslateFilter('es', 'en', percentage=0.3),
     autoblog.filters.discard.HTMLIds(),
     autoblog.filters.discard.AfterString('<p>No related posts.</p>'),
     autoblog.filters.discard.AfterString('<div class="blogger-post-footer">'),
     autoblog.filters.discard.AfterString('<p>Related posts:</p>'),
     autoblog.filters.discard.AfterString('<strong>Author:</strong>'),
     autoblog.filters.discard.AfterString('Filed under:'),
+    autoblog.filters.rewrite.GoogleTranslateFilter('es', 'en', percentage=0.3),
     autoblog.filters.misc.ValidHTML(),
 ]
 
 SITE_DIR = os.path.abspath(os.path.dirname(__file__))
-POST_DIR = os.path.join(SITE_DIR, '_posts')
+POST_DIR = os.path.join(SITE_DIR, 'source', '_posts')
 BUILD_DIR = os.path.join(SITE_DIR, '_site')
 TEMPLATES = jinja2.Environment(loader=jinja2.PackageLoader('autoblog', 'templates'))
-REMOTE = 'CHANGEME@CHANGEME:CHANGEME/'
-FEED_URL='http://CHANGEME/atom.xml'
+REMOTE = '/var/www/SITE/html/'
+FEED_URL='http://SITE/atom.xml'
